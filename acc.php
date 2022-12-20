1f2f5ad296da50869682289949cdf6d6dcfee13e
baka <dumenick.eldos@gmail.com>
2022-12-20 13:15:50 +0600

    NICE7

--- acc.php
+++ acc.php
@@ -36,23 +36,19 @@
             margin-left: 10px;
             font-size: 2vw;
         }
-        .up{
-            color: white;
-            font-family: 'Inter';
-            font-style: normal;
-            font-weight: 700;
-            margin-left: 10px;
-            font-size: 2vw;
-        }a{
+        a{
             text-decoration-line:none ;
         }
         .box{
-            margin: 7%;
+            margin-top: 7%;
             text-align: center;
             color: black;
         }
         #ex{
             font-size:30px;
+        }
+        .big{
+            font-size:40px;
         }
         
         
@@ -62,11 +58,11 @@
 </head>
 <body>
     <header>
-        <a href="info.php"><h2 class="up">Info</h2></a>
-        <a href="Catalog.php"><h2 class="up">Catalog</h2></a>
+        <a href="info.php"><h2 class="Title">Info</h2></a>
+        <a href="Catalog.php"><h2 class="Title">Catalog</h2></a>
         <a href="index.php"><h1 class="Title">SDU STORE</h1></a>
-        <a href="Basket.php"><h2 class="up">Basket</h2></a>
-        <a href="reg.php"> <h2 class="up">Account</h2> </a>
+        <a href="Basket.php"><h2 class="Title">Basket</h2></a>
+        <a href="reg.php"> <h2 class="Title">Account</h2> </a>
     </header>
     <div class="box">
     <?php
@@ -76,11 +72,11 @@
         $stmt = $pdo->prepare("SELECT * FROM user where login = '$login' ");
         $stmt->execute();
         $res = $stmt->fetchAll();
-        print_r ('<H1>Login: '.$res[0]['Login']."</H1><br>");
-        print_r ('<H1>Password: '.$res[0]['Password']."</H1><br>");
-        print_r ('<H1>Password: '.$res[0]['Name']."</H1><br>");
-        print_r ('<H1>Password: '.$res[0]['Surname']."</H1><br>");
-        print_r ('<H1>Password: '.$res[0]['Number']."</H1><br>");
+        print_r ('<p class = "big">Login: '.$res[0]['Login']."</H2><br>");
+        print_r ('<p class = "big">Password: '.$res[0]['Password']."</H2><br>");
+        print_r ('<p class = "big">Password: '.$res[0]['Name']."</H2><br>");
+        print_r ('<p class = "big">Password: '.$res[0]['Surname']."</H2><br>");
+        print_r ('<p class = "big">Password: '.$res[0]['Number']."</H2><br>");
         ?>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 
